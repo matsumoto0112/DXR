8a6033ab1ad936a4578fbf2c55466f6122d49da1
matsumoto <matsumoto.k017a1377@gmail.com>
2019-11-01 22:58:28 +0900

    GLTFSDKのインポート

--- Application/Application/Source/stdafx.h
+++ Application/Application/Source/stdafx.h
@@ -4,5 +4,18 @@
 */
 
 #pragma once
+#include "targetvar.h"
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#define NOMINMAX
 #include <Windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include "Libs/d3dx12.h"
+
+
+#include <GLTFSDK/GLTF.h>
---
+++ Application/Application/Source/targetvar.h
@@ -0,0 +1,9 @@
+/**
+* @file targetvar.h
+* @brief WindowsSDKのバージョン指定
+*/
+
+#pragma once
+
+//最新バージョンを使用
+#include <sdkddkver.h>
