dd95ded30b2495e6f053085d58b584d709c01acc
matsumoto <matsumoto.k017a1377@gmail.com>
2019-11-28 20:11:17 +0900

    ファイルのヘッダーの作成

--- Application/Application/Source/DX/Raytracing/DXRPipelineStateObject.h
+++ Application/Application/Source/DX/Raytracing/DXRPipelineStateObject.h
@@ -1,3 +1,8 @@
+/**
+ * @file DXRPipelineStateObject.h
+ * @brief レイトレーシング用パイプライン
+ */
+
 #pragma once
 
 namespace Framework::DX {
@@ -18,7 +23,9 @@ namespace Framework::DX {
             std::wstring closestHitName; //!< closesthitシェーダー名
             std::wstring anyHitName; //!< anyhitシェーダー名
             std::wstring intersectionName; //!< intersectionシェーダー名
-
+            /**
+             * @brief コンストラクタ
+             */
             HitGroupDesc(const std::wstring& name, D3D12_HIT_GROUP_TYPE type,
                 const std::wstring& closestHitName = L"", const std::wstring& anyHitName = L"",
                 const std::wstring& intersectionName = L"")
@@ -38,7 +45,6 @@ namespace Framework::DX {
          * @brief デストラクタ
          */
         ~DXRPipelineStateObject();
-
         /**
          * @brief シェーダーファイルからシェーダーを取り出す
          * @param shaderCode シェーダーコード
@@ -66,23 +72,30 @@ namespace Framework::DX {
          * @brief グローバルルートシグネチャのバインドをする
          */
         void bindGlobalRootSignature(const RootSignature& rootSignature);
-
+        /**
+         * @brief パイプラインの作成
+         */
         void create(ID3D12Device5* device);
-
+        /**
+         * @brief パイプラインの取得
+         */
         ID3D12StateObject* getStateObject() const {
             return mPipelineStateObject.Get();
         }
 
     private:
-        CD3DX12_STATE_OBJECT_DESC mPipelineStateObjectDesc;
-        ComPtr<ID3D12StateObject> mPipelineStateObject;
+        CD3DX12_STATE_OBJECT_DESC mPipelineStateObjectDesc; //!< パイプラインディスク
+        ComPtr<ID3D12StateObject> mPipelineStateObject; //!< パイプラインオブジェクト
     };
+
+    //シェーダーの読み込み
     template <class... T>
     inline void DXRPipelineStateObject::exportShader(
         void* shaderCode, size_t byteLength, const T&... names) {
         CD3DX12_DXIL_LIBRARY_SUBOBJECT* lib
             = mPipelineStateObjectDesc.CreateSubobject<CD3DX12_DXIL_LIBRARY_SUBOBJECT>();
         lib->SetDXILLibrary(&CD3DX12_SHADER_BYTECODE(shaderCode, byteLength));
+
         for (auto&& name : std::initializer_list<std::wstring>{ names... }) {
             lib->DefineExport(name.c_str());
         }
